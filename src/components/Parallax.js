13787cc373a5ff2f3a4c6600d68d5da2e8eea7d5
Raimon <ramonkoudsi@gmail.com>
2020-05-03 14:15:04 -0930

    nombres

--- src/components/Parallax.js
+++ src/components/Parallax.js
@@ -3,6 +3,7 @@ import {Parallax, ParallaxLayer} from 'react-spring/renderprops-addons';
 import bgCelda from '../imgs/background/3.png';
 import Title from './Title';
 import Slider from './Slider';
+//chaNGES
 
 import ReactResizeDetector from 'react-resize-detector';
 
--- src/components/header.js
+++ src/components/header.js
@@ -3,6 +3,8 @@ import {Navbar, Nav} from 'react-bootstrap';
 
 import Icono from "../imgs/header/wallbang-logo.png";
 
+//changes
+
 
 export default () => (
         <Navbar className="fixed-top navbar-dark headerWallbang" expand="lg">
