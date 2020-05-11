fcb062cc6df630485e46d3518fbc8e889ba813a4
Raimon <ramonkoudsi@gmail.com>
2020-05-10 22:52:34 -0930

    Update about.js

--- src/components/about.js
+++ src/components/about.js
@@ -8,9 +8,9 @@ export default () => {
     <div className="page-about">
             <header className="text-center">
                 <h2 className="header-repositories">ABOUT THE GAME</h2>
-                <h3 className="text-repositories">Catarsis is a Post-Apocaliptic Shoot n' fight where you live the broken politics and fight against the humans who transforms in their worst fears, as you try to recover any sign of identity within yourself.</h3>
+                <h3 className="text-repositories"  id="team">Catarsis is a Post-Apocaliptic Shoot n' fight where you live the broken politics and fight against the humans who transforms in their worst fears, as you try to recover any sign of identity within yourself.</h3>
             </header>
-            <ul className="container-repositories" id="team">
+            <ul className="container-repositories">
                 {
                     repos.map((repo)=>{
                     return <Repo repo= {repo} key={repo.id} />
