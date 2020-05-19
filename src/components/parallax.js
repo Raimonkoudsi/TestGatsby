c49f79ab3dd54353498787fd29d80b404f4942c9
Raimon <ramonkoudsi@gmail.com>
2020-05-19 13:55:39 -0930

    Update parallax.js

--- src/components/parallax.js
+++ src/components/parallax.js
@@ -117,22 +117,27 @@ class ParallaxBackground extends React.Component {
   }
 
 
+
+  menuCharge = () => {
+    if (this.state.isShow!='undefined' || this.state.isShowTeam!='undefined' || this.state.isShowContact!='undefined') {
+      let menu = <Header variable={this.state.isShow} variableTeam={this.state.isShowTeam} variableContact={this.state.isShowContact}/>;
+    } else {
+      let menu = null;
+    }
+
+    return this.menu;
+  }
+
+
     render(){
       var offset=this.offsetY;
-      let menu;
-
-      if (this.state.isShow!='undefined' || this.state.isShowTeam!='undefined' || this.state.isShowContact!='undefined') {
-        menu = <Header variable={this.state.isShow} variableTeam={this.state.isShowTeam} variableContact={this.state.isShowContact}/>;
-      } else {
-        menu = null;
-      }
 
     return(
 
     <div>
 
       <ReactResizeDetector handleWidth handleHeight onResize={this.onResize}> 
-        {menu}        
+        {this.menuCharge}        
         <Parallax pages={offset} scrolling={true} vertical ref={ref => (this.parallax = ref)}>
 
         <ParallaxLayer offset={0} speed={-1}>
