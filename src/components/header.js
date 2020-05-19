e19e0234b0d1b0dc228a956c59f615008c161213
Raimon <ramonkoudsi@gmail.com>
2020-05-19 08:22:45 -0930

    quitando todos los routes

--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,9 @@ import {Navbar, Nav} from 'react-bootstrap';
 
 import Icono from "../imgs/header/wallbang-logo.png";
 
-import { NavHashLink as NavLink } from 'react-router-hash-link';
+//import { NavHashLink as NavLink } from 'react-router-hash-link';
+
+import { Link } from '@reach/router';
 
 
 class Menu extends React.Component {
@@ -20,9 +22,9 @@ class Menu extends React.Component {
         <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
             </Nav>
-            <Nav.Link><NavLink to="#about" exact smooth className={this.props.variable ? "headerActive" : "headerLinks"}>About</NavLink></Nav.Link>
-            <Nav.Link><NavLink to="#team" exact smooth className={this.props.variableTeam ? "headerActive" : "headerLinks"}>Team</NavLink></Nav.Link>
-            <Nav.Link><NavLink to="#contact" exact smooth className={this.props.variableContact ? "headerActive" : "headerLinks"}>Contact</NavLink></Nav.Link>
+            <Nav.Link><Link to="#about" exact smooth className={this.props.variable ? "headerActive" : "headerLinks"}>About</Link></Nav.Link>
+            <Nav.Link><Link to="#team" exact smooth className={this.props.variableTeam ? "headerActive" : "headerLinks"}>Team</Link></Nav.Link>
+            <Nav.Link><Link to="#contact" exact smooth className={this.props.variableContact ? "headerActive" : "headerLinks"}>Contact</Link></Nav.Link>
         </Navbar.Collapse>
         </Navbar>
         )
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,18 +2,13 @@ import React from "react";
 import Header from "../components/header";
 import Parallax from "../components/parallax";
 
-import {
-    BrowserRouter as Router
-  } from "react-router-dom";
 
 export default () => (
 
 
 
     <div>
-        <Router>
             <Parallax />
-        </Router>
     </div>
 
 )
